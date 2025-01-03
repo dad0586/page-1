4f8bf5793c3782daa44290c926d16bebe6ce7350
otaboy <rakhmonovotaboy0586@gmail.com>
2025-01-03 16:26:01 +0500

    new days -1

--- Desktop/reactA/page/src/components/card/index.jsx
+++ Desktop/reactA/page/src/components/card/index.jsx
@@ -4,7 +4,13 @@ const Card = () => {
     return (
         <>
             <div className="card">
-                
+
+
+                <div>
+                    <h1>
+                        
+                    </h1>
+                </div>
             </div>
         </>
     )
