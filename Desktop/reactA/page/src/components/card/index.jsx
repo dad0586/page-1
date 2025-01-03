0070aa239397a64ff88bd02a6dcaffaf5499136c
otaboy <rakhmonovotaboy0586@gmail.com>
2025-01-03 16:54:11 +0500

    explore -1

--- Desktop/reactA/page/src/components/card/index.jsx
+++ Desktop/reactA/page/src/components/card/index.jsx
@@ -4,12 +4,27 @@ const Card = () => {
     return (
         <>
             <div className="card">
-
-
                 <div>
                     <h1>
-                        
+                        Car name
                     </h1>
+                    <p>
+
+                    </p>
+                </div>
+                <div>
+                    <img src="" alt="Car image" />
+                </div>
+                <div>
+                    <div>90L</div>
+                    <div>Manual</div>
+                    <div>People</div>
+                </div>
+                <div>
+                    <div>Prise: / <span>day</span> </div>
+                    <div>
+                        <button>Rent Now</button>
+                    </div>
                 </div>
             </div>
         </>
